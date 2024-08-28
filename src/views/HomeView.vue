3191484c3d89b7e1101386a7fa0a459d528f7fd3
Kamil Budzik <kbudzik@tbscg.com>
2024-08-28 19:41:33 +0200

    Project clean-up

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+console.log('SIEMANO KORANO')
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    SIEMANO KOLANO FORTPEIAN ELO
   </main>
 </template>
